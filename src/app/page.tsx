17b751a14efd0c4484e06649bf4f9bfc4242e03f
yooooonk <gisele51.jo@gmail.com>
2025-07-26 00:07:08 +0900

    [dashboard] portfolio 데이터 fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   mockPortfolioPie,
   mockPortfolioYield,
@@ -7,10 +7,28 @@ import {
 } from "../mock/portfolio";
 import PortfolioSummary from "@/components/PortfolioSummary";
 import PortfolioList from "@/components/PortfolioList";
+import { StockItem } from "@/type/stock";
 
-export default function Home() {
+export default function Dashboard() {
   const [tab, setTab] = useState<"비중" | "수익률">("비중");
   const pie = tab === "비중" ? mockPortfolioPie : mockPortfolioYield;
+
+  const [portfolioData, setPortfolioData] = useState<StockItem[]>([]);
+
+  useEffect(() => {
+    // 실제 API 호출
+    fetch("/api/portfolio")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+
+        setPortfolioData(data);
+      })
+      .catch((err) => {
+        // 에러 핸들링
+        console.error("포트폴리오 데이터 불러오기 실패:", err);
+      });
+  }, []);
 
   return (
     <>
